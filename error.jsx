e3b751b9ea9bc0958f6926149f66965892174476
sangmin131 <smha77@naver.com>
2023-01-20 21:16:36 +0900

    리액트 다운 error 저장

--- error.jsx
+++ error.jsx
@@ -1,6 +1,19 @@
+// 리액트 다운 순서
+// Node.js다운 받기 
+// 열린 명령창 혹은 터미널에 "npm -v"입력하여 잘 설치되었는지 확인.
+// create-react-app 설치 -> npm install -g create-react-app
+// create-react-app 설치 확인-> create-react-app -V
+
+// 오류 발생시 !!
+// 'react-scripts'은(는) 내부 또는 외부 명령, 실행할 수 있는 프로그램, 또는 배치 파일이 아닙니다.
+// 첫 다운로드시 npm start가 안된다면 npm update를 해보자 
+
+
+
 // 처음 시작해서 localhost3000 연결 안될때
 // 경로 지정하기 컨트롤 ` 누르고 cd (프로젝트 경로)하면 경로지정
 // 경로지정 후 npm start 를 해주면 리액트 사용준비 완료!
+
 
 // 컴포넌트를 불러올때 자동완성으로 입력하면 위에 import Comment from "경로";
 // 맨 아래는 내보내야하니까 export default CommentList; 
